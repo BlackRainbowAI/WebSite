a7c9bb7405bb3ba09b3a77b3bab270ba5dcb0653
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-22 05:05:47 +0200

    squash!

--- src/scripts/firebase.ts
+++ src/scripts/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyDJd7RlAawSDTDbLgxrGeoJcGJRtJxvYoo",
-	appId: "1:459237842628:web:5933d0f00a6202b6863391",
 	authDomain: "black-rainbow.firebaseapp.com",
-	measurementId: "G-96FE9JQEFV",
-	messagingSenderId: "459237842628",
-	projectId: "black-rainbow",
+  	projectId: "black-rainbow",
 	storageBucket: "black-rainbow.appspot.com",
+	messagingSenderId: "459237842628",
+	appId: "1:459237842628:web:5933d0f00a6202b6863391",
+	measurementId: "G-96FE9JQEFV"
 };
 
 initializeApp(firebaseConfig);
