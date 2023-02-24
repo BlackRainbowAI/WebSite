2b0957af61f495ec395436672281d855d79696f4
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-24 04:05:00 +0200

    Prettifies everything

--- src/scripts/firebase.ts
+++ src/scripts/firebase.ts
@@ -3,11 +3,11 @@ import { initializeApp } from "firebase/app";
 const firebaseConfig = {
 	apiKey: "AIzaSyDJd7RlAawSDTDbLgxrGeoJcGJRtJxvYoo",
 	authDomain: "black-rainbow.firebaseapp.com",
-  	projectId: "black-rainbow",
+	projectId: "black-rainbow",
 	storageBucket: "black-rainbow.appspot.com",
 	messagingSenderId: "459237842628",
 	appId: "1:459237842628:web:5933d0f00a6202b6863391",
-	measurementId: "G-96FE9JQEFV"
+	measurementId: "G-96FE9JQEFV",
 };
 
 initializeApp(firebaseConfig);
